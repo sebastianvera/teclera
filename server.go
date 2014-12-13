91c0e515530b15a001e891f5366f4f2828f4f640
Sebastian Vera <u.verainf@gmail.com>
2014-12-13 16:39:00 -0300

    Read responses and save the answer

--- server.go
+++ server.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/cors"
 	"github.com/martini-contrib/render"
+	"github.com/tarm/goserial"
 )
 
-const MoteDevices int = 10
+const MoteDevices int = 2
 
 type Response struct {
-	Value int
-	From  int
+	Value int `json:"buttonPressed"`
+	From  int `json:"address"`
 }
 
 func (r *Response) Reset() {
@@ -67,6 +73,8 @@ func main() {
 	//TODO: Run serial daemon
 	//TODO: Make sure that the serial daemon adds the questions
 
+	resetQuestionsResponses() // Remove this line
+	go CheckSerial()
 	m.Post("/upload", upload)
 	m.Get("/uploads", listFiles)
 	m.Post("/questions/start/:type", startQuestion)
@@ -75,6 +83,64 @@ func main() {
 	m.Run()
 }
 
+func handleResponse(bytes []byte) {
+	res := Response{}
+	json.Unmarshal(bytes, &res)
+
+	// if is not set,
+	if !Responses[res.From-1].Answered() {
+		Responses[res.From-1] = res
+		fmt.Println("New response")
+		fmt.Printf("%+v\n", res)
+	} else {
+		fmt.Println("Response already answered")
+	}
+	fmt.Println("Responses:")
+	for _, i := range Responses {
+		fmt.Printf("%+v\n", i)
+	}
+}
+
+func CheckSerial() {
+	c := &serial.Config{Name: findArduino(), Baud: 9600}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+	buff := bufio.NewReader(s)
+	for {
+		bytes, _, err := buff.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		handleResponse(bytes)
+	}
+}
+
+func findArduino() string {
+	contents, _ := ioutil.ReadDir("/dev")
+
+	for _, f := range contents {
+		os := runtime.GOOS
+		switch os {
+		case "linux":
+			if strings.Contains(f.Name(), "ACM") ||
+				strings.Contains(f.Name(), "ttyUSB3") {
+				return "/dev/" + f.Name()
+			}
+		case "darwin":
+			if strings.Contains(f.Name(), "tty.usbmodem") ||
+				strings.Contains(f.Name(), "ttyUSB") {
+				return "/dev/" + f.Name()
+			}
+		default:
+			fmt.Errorf("Unknown Operating System: %s", os)
+		}
+	}
+
+	return ""
+}
 func test(r render.Render, params martini.Params) {
 	index, _ := strconv.Atoi(params["index"])
 	val, _ := strconv.Atoi(params["val"])
@@ -164,7 +230,9 @@ func listFiles(r render.Render) {
 	fileNames := []string{}
 
 	for _, file := range files {
-		fileNames = append(fileNames, file.Name())
+		if strings.Contains(file.Name(), ".pdf") {
+			fileNames = append(fileNames, file.Name())
+		}
 	}
 
 	if err != nil {
